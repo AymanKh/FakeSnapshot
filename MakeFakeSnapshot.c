e3c26846f4fde17f612cc35970436bbd3d4663bf
mb <mubil@kth.se>
2017-04-27 10:40:58 +0300

    Snapshot with Atlas

--- MakeFakeSnapshot.c
+++ MakeFakeSnapshot.c
@@ -4,7 +4,12 @@
 #include <stdlib.h>
 #include "MakeFakeSnapshot.h"
 
+#include "atlas_alloc.h"
+#include "atlas_api.h"
 
+
+uint32_t global_rgn_id;
+Snapshot ** snapshotArr; 
 
 void MakeSnapshotObject()
 {
@@ -26,7 +31,8 @@ void PopulateCstructs()
 	jint localBalance = (*env)->GetIntField(env, snapshot1, localBalance_fid);
 	jint moneyInTransfer = (*env)->GetIntField(env, snapshot1, moneyInTransfer_fid);
 
-	snapshot1_C = malloc(sizeof(*snapshot1_C));
+	snapshot1_C = (Snapshot *) nvm_alloc(sizeof(Snapshot), global_rgn_id);
+	//snapshot1_C = malloc(sizeof(*snapshot1_C));
 	snapshot1_C->id = id;
 	snapshot1_C->localBalance = localBalance;
 	snapshot1_C->moneyInTransfer = moneyInTransfer;
@@ -37,7 +43,8 @@ void PopulateCstructs()
 	localBalance = (*env)->GetIntField(env, snapshot2, localBalance_fid);
 	moneyInTransfer = (*env)->GetIntField(env, snapshot2, moneyInTransfer_fid);
 
-	snapshot2_C = malloc(sizeof(*snapshot2_C));
+	snapshot2_C = (Snapshot *) nvm_alloc(sizeof(Snapshot), global_rgn_id);
+	//snapshot2_C = malloc(sizeof(*snapshot2_C));
 	snapshot2_C->id = id;
 	snapshot2_C->localBalance = localBalance;
 	snapshot2_C->moneyInTransfer = moneyInTransfer;
@@ -48,26 +55,53 @@ void PopulateCstructs()
 	localBalance = (*env)->GetIntField(env, snapshot3, localBalance_fid);
 	moneyInTransfer = (*env)->GetIntField(env, snapshot3, moneyInTransfer_fid);
 
-	snapshot3_C = malloc(sizeof(*snapshot3_C));
+	snapshot3_C = (Snapshot *) nvm_alloc(sizeof(Snapshot), global_rgn_id);
+	//snapshot3_C = malloc(sizeof(*snapshot3_C));
 	snapshot3_C->id = id;
 	snapshot3_C->localBalance = localBalance;
 	snapshot3_C->moneyInTransfer = moneyInTransfer;
 
 	printf("%d %d %d\n",(int) snapshot3_C->id, (int) snapshot3_C->localBalance, (int) snapshot3_C->moneyInTransfer);
-
+	
+	snapshotArr[0] = snapshot1_C;
+	snapshotArr[1] = snapshot2_C;
+	snapshotArr[2] = snapshot3_C;
 }
 
 void freeStructs()
-{
-	free(snapshot1_C);
-	free(snapshot2_C);
-	free(snapshot3_C);
+{	
+	printf("Free snapshot 1 \n");
+	nvm_free(snapshot1_C);
+	printf("Free snapshot 2 \n");
+	nvm_free(snapshot2_C);
+	printf("Free snapshot 3 \n");
+	nvm_free(snapshot3_C);
+}
+
+int initialize(){
+
+	void *rgn_root = NVM_GetRegionRoot(global_rgn_id);
+	if(rgn_root){
+		snapshotArr = (Snapshot **) rgn_root;
+		return 1;
+	}else{
+		snapshotArr = (Snapshot **) nvm_alloc(3*sizeof(Snapshot*),  global_rgn_id);
+		return 0;
+	}
+}
+
+void print_snapshots(){
+
+	for (int i =0; i<3; i++){
+	Snapshot * sn = snapshotArr[i];
+		printf("Id number %d \n", (int) sn->id);
+	}
 }
 
 int main()
 {
 
-	options[0].optionString = "-Djava.class.path=./";
+	options[0].optionString = "-Djava.class.path=/home/mb/Documents/Atlas/runtime/tests/data_structures/";
 
 	memset(&vm_args, 0, sizeof(vm_args));
 	vm_args.version = JNI_VERSION_1_8;
@@ -90,12 +124,32 @@ int main()
 
 		MakeSnapshotObject();
 		GetFieldIDs();
-		PopulateCstructs();
+	
+		NVM_Initialize();
+		global_rgn_id = NVM_FindOrCreateRegion("queue", O_RDWR, NULL);		
+	
+		int status = initialize();
+		if (status){
+			printf("Root region found \n");
+			
+			
+			//printf("Recovered ID %d \n", (int)snapshot1_C->id);
+		}
+		else{
+			printf("No prior root found \n");
+			PopulateCstructs();
+		}
+		NVM_SetRegionRoot(global_rgn_id, snapshotArr);
+		print_snapshots();
 
 		// now the snapshots are stored in snapshot1_C, snapshot2_C, snapshot3_C
-
+		printf("Destroying JVM \n");
 		(*jvm)->DestroyJavaVM(jvm);
+		printf("Freeing Structs\n");
 		freeStructs();
+		
+		NVM_CloseRegion(global_rgn_id);
+		NVM_Finalize();
 		return 0;
 	}
 
